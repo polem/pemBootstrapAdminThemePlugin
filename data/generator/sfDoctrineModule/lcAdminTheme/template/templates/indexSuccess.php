ac295c5eab2b38e662af6484a95e055bf10333d1
Paul-Emile MINY <pminy@lacompany.fr>
2011-10-21 16:48:24 +0200

    fixes style

--- data/generator/sfDoctrineModule/lcAdminTheme/template/templates/indexSuccess.php
+++ data/generator/sfDoctrineModule/lcAdminTheme/template/templates/indexSuccess.php
@@ -25,10 +25,10 @@
 <?php endif; ?>
     [?php include_partial('<?php echo $this->getModuleName() ?>/list', array('pager' => $pager, 'sort' => $sort, 'helper' => $helper)) ?]
     <div class="clearfix">
-        <ul class="sf_admin_actions nav">
+        <div class="sf_admin_actions">
           [?php include_partial('<?php echo $this->getModuleName() ?>/list_batch_actions', array('helper' => $helper)) ?]
           [?php include_partial('<?php echo $this->getModuleName() ?>/list_actions', array('helper' => $helper)) ?]
-        </ul>
+        </div>
     </div>
 <?php if ($this->configuration->getValue('list.batch_actions')): ?>
     </form>
