8d9d9cd5c36263f68ec6c6fc512b4133f78340cd
Paul-Emile MINY <pminy@lacompany.fr>
2012-02-29 15:35:48 +0100

    updated flsh alerts

--- data/generator/sfDoctrineModule/lcAdminTheme/template/templates/_flashes.php
+++ data/generator/sfDoctrineModule/lcAdminTheme/template/templates/_flashes.php
@@ -1,7 +1,13 @@
 [?php if ($sf_user->hasFlash('notice')): ?]
-  <div class="alert-message info"><p>[?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]</p></div>
+  <div class="alert">
+    <a class="close" data-dismiss="alert" href="#">&times;</a>
+    [?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]
+  </div>
 [?php endif; ?]
 
 [?php if ($sf_user->hasFlash('error')): ?]
-  <div class="alert-message error"><p>[?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]</p></div>
+  <div class="alert alert-error">
+    <a class="close" data-dismiss="alert" href="#">&times;</a>
+    [?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]
+  </div>
 [?php endif; ?]
