b2f62b7b5583f2dab43a6d62d02ed70f06337f0e
Paul-Emile MINY <pminy@lacompany.fr>
2012-02-29 21:13:41 +0100

    wip bootstrap 2

--- data/generator/sfDoctrineModule/lcAdminTheme/template/lib/helper.php
+++ data/generator/sfDoctrineModule/lcAdminTheme/template/lib/helper.php
@@ -14,4 +14,44 @@ abstract class Base<?php echo ucfirst($this->getModuleName()) ?>GeneratorHelper 
   {
     return 'list' == $action ? '<?php echo $this->params['route_prefix'] ?>' : '<?php echo $this->params['route_prefix'] ?>_'.$action;
   }
+  public function linkToNew($params)
+  {
+    return '<span class="sf_admin_action_new">'.link_to('<i class="icon-plus icon-white"></i> '.__($params['label'], array(), 'sf_admin'), '@'.$this->getUrlForAction('new'), array('class' => 'btn btn-success')).'</span>';
+  }
+
+  public function linkToEdit($object, $params)
+  {
+    return '<span class="sf_admin_action_edit">'.link_to('<i class="icon-pencil"></i> '.__($params['label'], array(), 'sf_admin'), $this->getUrlForAction('edit'), $object, array('class' => 'btn')).'</span>';
+  }
+
+  public function linkToDelete($object, $params)
+  {
+    if ($object->isNew())
+    {
+      return '';
+    }
+
+    return '<span class="sf_admin_action_delete">'.link_to('<i class="icon-trash icon-white"></i> '.__($params['label'], array(), 'sf_admin'), $this->getUrlForAction('delete'), $object, array('method' => 'delete', 'confirm' => !empty($params['confirm']) ? __($params['confirm'], array(), 'sf_admin') : $params['confirm'], 'class' => 'btn btn-danger')).'</span>';
+  }
+
+  public function linkToList($params)
+  {
+    return '<span class="sf_admin_action_list">'.link_to(__($params['label'], array(), 'sf_admin'), '@'.$this->getUrlForAction('list'), array('class' => 'btn')).'</span>';
+  }
+
+  public function linkToSave($object, $params)
+  {
+    return '<span class="sf_admin_action_save"><input type="submit" class="btn btn-success" value="'.__($params['label'], array(), 'sf_admin').'" /></span>';
+  }
+
+  public function linkToSaveAndAdd($object, $params)
+  {
+    if (!$object->isNew())
+    {
+      return '';
+    }
+
+    return '<span class="sf_admin_action_save_and_add"><input type="submit" class="btn btn-success" value="'.__($params['label'], array(), 'sf_admin').'" name="_save_and_add" /></span>';
+  }
+
 }
--- data/generator/sfDoctrineModule/lcAdminTheme/template/templates/_form_actions.php
+++ data/generator/sfDoctrineModule/lcAdminTheme/template/templates/_form_actions.php
@@ -1,4 +1,4 @@
-<ul class="sf_admin_actions actions unstyled">
+<div class="sf_admin_actions form-actions unstyled">
 <?php foreach (array('new', 'edit') as $action): ?>
 <?php if ('new' == $action): ?>
 [?php if ($form->isNew()): ?]
@@ -19,7 +19,7 @@
   <?php echo $this->addCredentialCondition('[?php echo $helper->linkToSaveAndAdd($form->getObject(), '.$this->asPhp($params).') ?]', $params) ?>
 
 <?php else: ?>
-  <li class="sf_admin_action_<?php echo $params['class_suffix'] ?>">
+  <span class="sf_admin_action_<?php echo $params['class_suffix'] ?>">
 [?php if (method_exists($helper, 'linkTo<?php echo $method = ucfirst(sfInflector::camelize($name)) ?>')): ?]
   <?php echo $this->addCredentialCondition('[?php echo $helper->linkTo'.$method.'($form->getObject(), '.$this->asPhp($params).') ?]', $params) ?>
 
@@ -27,9 +27,9 @@
   <?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, true), $params) ?>
 
 [?php endif; ?]
-  </li>
+  </span>
 <?php endif; ?>
 <?php endforeach; ?>
 <?php endforeach; ?>
 [?php endif; ?]
-</ul>
+</div>
